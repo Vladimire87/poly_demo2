4fc65117fba72845f1955fd2227ec9ef96cf5dc7
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-08-05 21:41:23 +0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   root "home#index"
 end
